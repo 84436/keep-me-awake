1148c18da1f65d551b8608928cfa8b3e054fab77
Thien Bui <thien.bui.84436@gmail.com>
2022-01-13 01:07:21 +0700

    add 8-hour (fixed) countdown

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
         <div id="app-element">
             <v-icon v-if="isAwake" size="128">mdi-white-balance-sunny</v-icon>
             <v-icon v-else size="128">mdi-power-sleep</v-icon>
-            <v-switch color="black" class="flex-grow-0 ma-0 pa-0" dense v-model="isAwake" label="Keep me awake" :hide-details="true"></v-switch>
+            <v-switch
+                color="black"
+                class="flex-grow-0 ma-0 pa-0"
+                dense
+                v-model="isAwake"
+                :label="switchLabel"
+                :hide-details="true"
+            ></v-switch>
             <video loop id="video-element" src="/blank.mp4"></video>
         </div>
     </v-app>
@@ -15,27 +22,62 @@ import { startVideo, stopVideo } from "./services/videoService";
 // import { startAudio, stopAudio } from "./services/audioService";
 import { FAVICONS, setFavicon } from "./services/faviconService";
 
+// TODO make this user-settable
+const DURATION = 28800; // 28800 seconds = 8 hours
+
 export default {
     name: "App",
     data: () => ({
         isAwake: false,
+        countdown: DURATION,
+        countdownTimeoutObj: null,
     }),
     computed: {
         theme() {
             return this.isAwake ? "light" : "dark";
         },
+        switchLabel() {
+            return `Keep me awake for ${this.formatTime(this.countdown)}`;
+        },
     },
     watch: {
+        countdown(newVal) {
+            if (newVal === 0) {
+                this.isAwake = false;
+                this.countdown = DURATION;
+            }
+        },
         isAwake(newVal) {
             if (newVal) {
-                setUpWakelock();
-                startVideo();
-                setFavicon(FAVICONS.AWAKE);
+                this.startKeepAwake();
             } else {
-                releaseWakelock();
-                stopVideo();
-                setFavicon(FAVICONS.SLEEP);
+                this.stopKeepAwake();
             }
+        },
+    },
+    methods: {
+        startTimer() {
+            this.countdown -= 1;
+            this.countdownTimeoutObj = setTimeout(this.startTimer, 1000);
+        },
+        stopTimer() {
+            clearTimeout(this.countdownTimeoutObj);
+        },
+        startKeepAwake() {
+            setUpWakelock();
+            startVideo();
+            setFavicon(FAVICONS.AWAKE);
+            this.countdown += 1; // we don't want to lose 1 second immediately on countdown starting
+            this.startTimer();
+        },
+        stopKeepAwake() {
+            releaseWakelock();
+            stopVideo();
+            setFavicon(FAVICONS.SLEEP);
+            this.stopTimer();
+        },
+        formatTime(seconds) {
+            return new Date(seconds * 1000).toISOString().substring(11, 19);
         },
     },
 };
